5bd8282a75d23e98d819c63bf21c8b7f772f6bcf
Nick Strayer <nick@mct.io>
2014-08-20 15:27:44 -0700

    nodeMap is solar radiation levels (data is messed up) but is a good example of a clean working d3node map.

--- nodeMap/nodeScript.js
+++ nodeMap/nodeScript.js
@@ -2,12 +2,6 @@
 var upperLeft = [-151,58], 
     bottomRight =[-54,10.5];
 
-//////////////////////////////////////////////////////////////////////////////////
-//load up the csv woith whatever data you are working with: 
-
-//d3.csv("data/randomGeoData.csv",function(data){  //Random Data
-//d3.csv("data/latLonIfiedData.csv",function(data){ //Natural Gas data
-d3.csv("data/renewableStations.csv",function(data){ //renewable energy stations
 
 //Use info from the window size to draw the svg:
 var margin = {top: 0, left: 0, bottom: 0, right: 0}
@@ -27,9 +21,8 @@ var projection = d3.geo.mercator()
 var path = d3.geo.path()
     .projection(projection);
 
-var //ulPoint = projection([-85,24]), //trying cuba for a sec
-    //brPoint = projection([-74,19]),
-    ulPoint = projection(upperLeft), 
+
+var ulPoint = projection(upperLeft), 
     brPoint = projection(bottomRight),
     s = 1 / Math.max((brPoint[0] - ulPoint[0]) / width, (brPoint[1] - ulPoint[1]) / height),
     t = [(width - s * (brPoint[0] + ulPoint[0])) / 2, (height - s * (brPoint[1] + ulPoint[1])) / 2];
@@ -39,131 +32,46 @@ projection
     .scale(s)
     .translate(t);
 
-
 //Code to deal with a resizing of the WebThing:
 var g = svg.append("g");
 
-d3.select(window).on('resize', function(){ resize() });
+//////////////////////////////////////////////////////////////////////////////////
+//load up the csv woith whatever data you are working with: 
+//d3.csv("renewableStations.csv",function(data){ //renewable energy stations
 
-function resize() {
-    width = parseInt(d3.select('body').style('width'));
-    height = parseInt(d3.select('body').style('height'));
-    //height = width * mapRatio;
+d3.csv("solarLevelsCounty.csv",function(data){
 
-    // resize the map container
-    svg
-        .attr('width', width)
-        .attr('height', height);
-/*
-    // update projection
-    var ulPoint = projection(upperLeft), 
-        brPoint = projection(bottomRight),
-        s = 1 / Math.max((brPoint[0] - ulPoint[0]) / width, (brPoint[1] - ulPoint[1]) / height),
-        t = [(width - s * (brPoint[0] + ulPoint[0])) / 2, (height - s * (brPoint[1] + ulPoint[1])) / 2];
+var minVal = d3.min(data,function(d){return d.annualAverage})  
+var maxVal = d3.max(data,function(d){return d.annualAverage})
 
-// Update the projection to use computed scale & translate.
-    projection
-        .scale(s)
-        .translate(t);t. 
+var brightness = d3.scale.log()
+    .domain( [minVal, maxVal] ) 
+    .range(["black", "gold"])
 
-    // resize the map
-    g.selectAll('path').attr('d', path);
- */   
-    //move the data-nodes as well
-    d3.selectAll("circle")
-        .attr("cx",function(d){
-            return projection([d.lon, d.lat])[0]
-        })
-        .attr("cy",function(d){
-             return projection([d.lon, d.lat])[1]
-        })
-}
-
-
-//Pulsating points: 
-var movingCircles = function(){
-    g.selectAll("circle")
-        .transition()
-        .duration(800)
-        .ease("log")
-        .attr("r", 5)
-        .attr("fill", "blue")
-        .each("end", function(){
-            d3.select(this)
-                .transition()
-                .duration(800)
-                .ease("log")
-                .attr("r", 3)
-                //.attr("fill", "red")
-                .each("end",function(){
-                    movingCircles()
-                })
-        })
-}
-
-// load and display the World
-d3.json("world-110m2.json", function(error, topology) {
-    g.selectAll("path")
-      .data(topojson.object(topology, topology.objects.countries)
-          .geometries)
-    .enter()
-      .append("path")
-      .attr("d", path)
-      .on("click", function(d){
-
-        console.log(projection.invert(path.bounds(d)[0])) //UL corner
-        console.log(projection.invert(path.bounds(d)[1])) //BR corner
-    })
-
-    g.selectAll("circle")
-        .data(data)
+    // load and display the World
+    d3.json("world-110m2.json", function(error, topology) {
+        g.selectAll("path")
+          .data(topojson.object(topology, topology.objects.countries)
+              .geometries)
         .enter()
-        .append("circle")
-        //.attr("cx", 0)
-        //.attr("cy", 0)
-        .on("mouseover",function(d){
-            d3.select(this)
-            .transition()
-            .duration(400)
-            .attr("r", 10)
+          .append("path")
+          .attr("d", path)
 
-            svg.append("text")
-                //.text(d["Plant Name"])
-                .text(d['Street Address'] + ", " + d['City'] + ", " + d["State"])
-                .attr("id", "plantNameText")
-                .attr("x",d3.select(this).attr("cx") - 10)
-                .attr("y",d3.select(this).attr("cy"))
-                .attr("text-anchor","end")
-                .attr("fill","blue")
-        })
-        .on("mouseout",function(){
-            d3.select(this)
-            .transition()
-            .duration(400)
-            .attr("r",2)
-
-            d3.selectAll("#plantNameText")
-                .remove()
-        })
-        //.transition()
-        //.duration(1000)
-        .attr("r", 2)
-        .attr("cx",function(d){
-          return projection([d.lon, d.lat])[0]
-        })
-        .attr("cy",function(d){
-          return projection([d.lon, d.lat])[1]
-        })
-        //.each("end",function(d,i){ movingCircles() })
-});
+        g.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("fill", function(d){return brightness(d.annualAverage)})
+            .attr("r", 7)
+            .attr("cx",function(d){
+              return projection([d.lon, d.lat])[0]
+            })
+            .attr("cy",function(d){
+              return projection([d.lon, d.lat])[1]
+            })
 
 
-svg.append("text")
-    .text("Natural Gas wells")
-    .attr("x", 15)
-    .attr("y", 40)
-    .attr("text-anchor","start")
-    .attr("font-size", 25)
+    });//closes .json load
 
-})
+})//closes csv load
 
