aa0c6244efa93bb6b099367f6b1b0e65511de66c
Nick Strayer <nick@mct.io>
2014-08-21 15:07:56 -0700

    Working hexbin of average solar radiation levels in /hexbinedSolarLevels

--- hexbinedSolarLevels/script.js
+++ hexbinedSolarLevels/script.js
@@ -21,9 +21,11 @@ var projection = d3.geo.mercator()
 var path = d3.geo.path()
     .projection(projection);
 
+var hexRadius = 15
+
 var hexbin = d3.hexbin()
     .size([width, height])
-    .radius(8);
+    .radius(hexRadius);
 
 var brightness = d3.scale.linear()
     .domain( [2, 8.2] ) 
@@ -34,6 +36,7 @@ var ulPoint = projection(upperLeft),
     s = 1 / Math.max((brPoint[0] - ulPoint[0]) / width, (brPoint[1] - ulPoint[1]) / height),
     t = [(width - s * (brPoint[0] + ulPoint[0])) / 2, (height - s * (brPoint[1] + ulPoint[1])) / 2];
 
+
 // Update the projection to use computed scale & translate.
 projection
     .scale(s)
@@ -42,37 +45,35 @@ projection
 //Code to deal with a resizing of the WebThing:
 var g = svg.append("g");
 
-// load and display the World
-d3.json("world-110m2.json", function(error, topology) {
-    g.selectAll("path")
+queue()
+        .defer(d3.json,"world-110m2.json")
+        .defer(d3.csv, "solarLevelsHexbin.csv")
+        .await(ready);
+
+function ready(error, topology, data) {
+  data.forEach(function(d){
+    var p = projection(d);
+    d[0] = p[0], d[1] = p[1]; 
+  });
+
+  g.selectAll("path")
       .data(topojson.object(topology, topology.objects.countries)
           .geometries)
     .enter()
       .append("path")
       .attr("d", path)
 
-    queue()
-        .defer(d3.csv, "cleanedSolarLevels.csv")
-        .await(ready);
-
-    function ready(error, data) {
-      data.forEach(function(d){
-        var p = projection(d);
-        d[0] = p[0], d[1] = p[1]; 
-      });
-
-    svg.append("g")
-        .attr("class", "hexagons")
-      .selectAll("path")
-        .data(hexbin(data).sort(function(a,b){return b.length - a.length; }))
-      .enter().append("path")
-        .attr("d", 8.5)
-        .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; })
-        .style("fill", function(d){ return brightness(d3.mean(d, function(d){return +d.annualAverage; }))})
-
+  svg.append("g")
+      .attr("class", "hexagons")
+    .selectAll("path")
+      .data(hexbin(data).sort(function(a,b){return b.length - a.length; }))
+    .enter().append("path")
+      .attr("d", hexbin.hexagon(0.1))
+      .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; })
+      .style("fill", function(d){ return brightness(d3.mean(d, function(d){return +d.annualAverage; }))})
+      .transition()
+      .duration(1000)
+      .attr("d", hexbin.hexagon(hexRadius - 0.5))
 }
-
-
-});//closes .json load
 
 
