2ad026042f3d6872f0e4ab93248ba71894740dc9
Nick Strayer <nick@mct.io>
2014-08-21 17:11:05 -0700

    got nuclear plants vis up and running.

--- nuclearPlants/script.js
+++ nuclearPlants/script.js
@@ -11,10 +11,6 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-//////////////////////////////////////////////////////////////////////////////////
-d3.csv("nuclearPowerPlants.csv",function(data){ //renewable energy stations
-
-//set up the projection: 
 var projection = d3.geo.mercator()
     .scale(1)
     .translate([0,0]);
@@ -22,7 +18,9 @@ var projection = d3.geo.mercator()
 var path = d3.geo.path()
     .projection(projection);
 
-var ulPoint = projection(upperLeft), 
+var //ulPoint = projection([-85,24]), //trying cuba for a sec
+    //brPoint = projection([-74,19]),
+    ulPoint = projection(upperLeft), 
     brPoint = projection(bottomRight),
     s = 1 / Math.max((brPoint[0] - ulPoint[0]) / width, (brPoint[1] - ulPoint[1]) / height),
     t = [(width - s * (brPoint[0] + ulPoint[0])) / 2, (height - s * (brPoint[1] + ulPoint[1])) / 2];
@@ -32,61 +30,51 @@ projection
     .scale(s)
     .translate(t);
 
+
 //Code to deal with a resizing of the WebThing:
 var g = svg.append("g");
 
-d3.select(window).on('resize', function(){ resize() });
+queue()
+        .defer(d3.json,"world-110m2.json")
+        .defer(d3.csv, "nuclearReactorsWorld.csv")
+        .await(ready);
 
-function resize() {
-    width = parseInt(d3.select('body').style('width'));
-    height = parseInt(d3.select('body').style('height'));
-    //height = width * mapRatio;
+function ready(error, topology, data) {
 
-    // resize the map container
-    svg
-        .attr('width', width)
-        .attr('height', height);
-
-    //move the data-nodes as well
-    d3.selectAll("circle")
-        .attr("cx",function(d){
-            return projection([d.lon, d.lat])[0]
-        })
-        .attr("cy",function(d){
-             return projection([d.lon, d.lat])[1]
-        })
-}
-
-
-// load and display the World
-d3.json("world-110m2.json", function(error, topology) {
     g.selectAll("path")
       .data(topojson.object(topology, topology.objects.countries)
           .geometries)
     .enter()
       .append("path")
       .attr("d", path)
-      .on("click", function(d){
 
-        console.log(projection.invert(path.bounds(d)[0])) //UL corner
-        console.log(projection.invert(path.bounds(d)[1])) //BR corner
-    })
-
-g.selectAll("circle")
+    g.selectAll("circle")
     .data(data)
     .enter()
     .append("circle")
     .attr("r", 4)
-    .attr("fill-opacity", 0.3)
-    .attr("fill", "steelblue")
+    .attr("fill-opacity", 0.8)
+    .attr("fill","steelblue")
     .attr("cx",function(d){
-      return projection(d['location'])[0]
+      return projection([d.lon, d.lat])[0]
     })
     .attr("cy",function(d){
-      return projection(d['location'])[1]
+      return projection([d.lon, d.lat])[1]
     })
 
-});
+}
 
+// zoom and pan
+var zoom = d3.behavior.zoom()
+    .on("zoom",function() {
+        g.attr("transform","translate("+ 
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g.selectAll("circle")
+            .attr("d", path.projection(projection));
+        g.selectAll("path")  
+            .attr("d", path.projection(projection)); 
 
-})
+  });
+
+svg.call(zoom)
+//////////////////////////////////////////////////////////////////////////////////
